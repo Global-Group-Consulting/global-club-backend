6c3d65165eb58cd72bbfaceb66eea3791965cc4f
leicaflorian <florian.leica@gmail.com>
2021-09-16 08:17:13 +0200

    Remove unused import

--- src/movements/movements.service.ts
+++ src/movements/movements.service.ts
@@ -1,4 +1,4 @@
-import {Model, Schema} from "mongoose";
+import {Model} from "mongoose";
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
 import {Movement, MovementDocument} from "./schemas/movement.schema";
