f4b36c858eb57787ae342d88af3bc5556a47deaa
leicaflorian <florian.leica@gmail.com>
2021-09-20 19:35:32 +0200

    Removes old imports

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,8 @@
 import {Module} from '@nestjs/common';
-import {HttpModule} from "@nestjs/axios";
 import {MongooseModule} from "@nestjs/mongoose";
 import {ProductsService} from './products.service';
 import {ProductsController} from './products.controller';
 import {Product, ProductSchema} from "./schemas/product.schema";
-import {HttpConfigService} from "../_configServices/HttpConfigService";
 import {AxiosModule} from "../axios/axios.module";
 
 @Module({
