bbca60694534259fade50e6bf0c0d0f9d6710b97
leicaflorian <florian.leica@gmail.com>
2021-09-22 17:34:07 +0200

    Connect orders to db

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
+import {Module} from '@nestjs/common';
+import {OrdersService} from './orders.service';
+import {OrdersController} from './orders.controller';
+import {MongooseModule} from "@nestjs/mongoose";
+import {Order, OrdersSchema} from "./schemas/order.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrdersSchema
+      }
+    ], "club")
+  ],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
-export class OrdersModule {}
+export class OrdersModule {
+}
