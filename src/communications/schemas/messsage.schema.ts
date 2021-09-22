97d5cb0dda3c5cefeea5e5db022aaa741fc6eb72
leicaflorian <florian.leica@gmail.com>
2021-09-22 17:26:29 +0200

    Adds attachments to MessageSchema

--- src/communications/schemas/messsage.schema.ts
+++ src/communications/schemas/messsage.schema.ts
@@ -3,18 +3,21 @@ import {BasicSchema} from "../../_basics/BasicSchema";
 import {Types} from "mongoose";
 import {User} from "../../users/entities/user.entity";
 import {MessageUser} from "./message.user.schema";
+import {AddMessageCommunicationDto} from "../dto/add-message-communication.dto";
+import {Attachment} from "../../_basics/attachment.entity";
 
 export class Message implements BasicSchema {
   sender: UserBasic = null
-  
-  content: string = ""
+  attachments: Attachment[] = null
+  content: string = null
   
   readonly _id: Types.ObjectId = new Types.ObjectId();
   readonly createdAt: Date = new Date();
   readonly updatedAt: Date = new Date();
   
-  constructor(message: string, sender: Partial<User>) {
-    this.content = message;
+  constructor(data: AddMessageCommunicationDto, sender: Partial<User>) {
+    this.content = data.message;
+    this.attachments = data.attachments;
     this.sender = new MessageUser(sender);
   }
 }
