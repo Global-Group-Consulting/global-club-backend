b64aa92839922639c69dcb0747be2206b3db4675
leicaflorian <florian.leica@gmail.com>
2021-09-22 17:26:37 +0200

    Adds validation to messages

--- src/communications/dto/add-message-communication.dto.ts
+++ src/communications/dto/add-message-communication.dto.ts
@@ -1,3 +1,14 @@
+import {Attachment} from "../../_basics/attachment.entity";
+import {IsArray, IsNotEmpty, IsOptional, ValidateNested} from "class-validator";
+import {Type} from "class-transformer";
+
 export class AddMessageCommunicationDto {
+  @IsNotEmpty()
   message: string = ""
+  
+  @IsOptional()
+  @IsArray()
+  @Type(() => Attachment)
+  @ValidateNested({each: true})
+  attachments: Attachment[]
 }
