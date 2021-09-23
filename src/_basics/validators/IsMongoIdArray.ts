0bcef7a1a5915175f0b2c5ac96484365fafdddc6
leicaflorian <florian.leica@gmail.com>
2021-09-23 18:13:37 +0200

    Adds IsMongoId validator for arrays

---
+++ src/_basics/validators/IsMongoIdArray.ts
@@ -0,0 +1,40 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  isMongoId
+} from 'class-validator';
+
+@ValidatorConstraint({async: true})
+export class IsMongoIdArrayConstraint implements ValidatorConstraintInterface {
+  validate(value: any[], args: ValidationArguments) {
+    for (const str of value) {
+      const isValid = isMongoId(str)
+      
+      if (!isValid) {
+        return false
+      }
+    }
+    
+    return true
+  }
+  
+  defaultMessage() {
+    return "$property must contain valid mongodb id's"
+  }
+}
+
+export function IsMongoIdArray(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isMongoIdArray',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: IsMongoIdArrayConstraint
+    });
+  };
+}
