6f29ee8e50b29b5309b766fcf31cdabea1218d9a
leicaflorian <florian.leica@gmail.com>
2021-09-24 18:49:43 +0200

    Updates attachments schema

--- src/_schemas/attachment.schema.ts
+++ src/_schemas/attachment.schema.ts
@@ -1,15 +1,25 @@
 import {IsNotEmpty} from "class-validator";
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 
+@Schema({
+  _id: false
+})
 export class Attachment {
   @IsNotEmpty()
+  @Prop()
   id: string;
   
   @IsNotEmpty()
+  @Prop()
   fileName: string;
   
   @IsNotEmpty()
+  @Prop()
   size: number;
   
   @IsNotEmpty()
+  @Prop()
   mimetype: string;
 }
+
+export const AttachmentSchema = SchemaFactory.createForClass(Attachment)
