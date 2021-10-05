7de7c7f2601f540809a4620475ae3403701138ba
--get <florian.leica@gmail.com>
2021-10-05 18:41:04 +0200

    Adds function for removing a single message from a conversation

--- src/communications/communications.service.ts
+++ src/communications/communications.service.ts
@@ -1,28 +1,30 @@
-import {Model} from "mongoose";
-import {REQUEST} from "@nestjs/core";
-import {InjectModel} from "@nestjs/mongoose";
-import {Inject, Injectable} from '@nestjs/common';
-import {CreateCommunicationDto} from './dto/create-communication.dto';
-import {Communication, CommunicationDocument} from "./schemas/communications.schema";
-import {User} from "../users/entities/user.entity";
-import {AuthRequest} from "../_basics/AuthRequest";
-import {FindException} from "../_exceptions/find.exception";
-import {AddMessageCommunicationDto} from "./dto/add-message-communication.dto";
-import {FilesService} from "../files/files.service";
-import {UpdateException} from "../_exceptions/update.exception";
+import { Model } from 'mongoose'
+import { REQUEST } from '@nestjs/core'
+import { InjectModel } from '@nestjs/mongoose'
+import { Inject, Injectable } from '@nestjs/common'
+import { CreateCommunicationDto } from './dto/create-communication.dto'
+import { Communication, CommunicationDocument } from './schemas/communications.schema'
+import { User } from '../users/entities/user.entity'
+import { AuthRequest } from '../_basics/AuthRequest'
+import { FindException } from '../_exceptions/find.exception'
+import { AddMessageCommunicationDto } from './dto/add-message-communication.dto'
+import { FilesService } from '../files/files.service'
+import { UpdateException } from '../_exceptions/update.exception'
+import { MessageTypeEnum } from './enums/message.type.enum'
+import { castToObjectId } from '../utilities/Formatters'
 
 @Injectable()
 export class CommunicationsService {
-  constructor(@InjectModel(Communication.name) private communicationModel: Model<CommunicationDocument>,
-              @Inject(REQUEST) private request: AuthRequest,
-              private filesService: FilesService) {
+  constructor (@InjectModel(Communication.name) private communicationModel: Model<CommunicationDocument>,
+    @Inject(REQUEST) private request: AuthRequest,
+    private filesService: FilesService) {
   }
   
-  get authUser(): User {
-    return this.request.auth.user;
+  get authUser (): User {
+    return this.request.auth.user
   }
   
-  async create(createCommunicationDto: CreateCommunicationDto) {
+  async create (createCommunicationDto: CreateCommunicationDto) {
     const newCommunication = new this.communicationModel({
       ...createCommunicationDto,
       messages: [{
@@ -36,31 +38,32 @@ export class CommunicationsService {
     return newCommunication.save()
   }
   
-  findAll(): Promise<Communication[]> {
-    return this.communicationModel.find().exec();
+  findAll (): Promise<Communication[]> {
+    return this.communicationModel.find().exec()
   }
   
-  findOne(id: string) {
-    return this.communicationModel.findById(id).exec();
+  findOne (id: string) {
+    return this.communicationModel.findById(id).exec()
   }
   
   /*update(id: string, updateCommunicationDto: UpdateCommunicationDto) {
     return null
   }*/
   
-  async addMessage(id: string, addMessageCommunicationDto: AddMessageCommunicationDto) {
-    const communication = await this.communicationModel.findById(id).exec();
+  async addMessage (id: string, addMessageCommunicationDto: AddMessageCommunicationDto, type?: MessageTypeEnum) {
+    const communication = await this.communicationModel.findById(id).exec()
     
     if (!communication) {
-      throw new UpdateException("Can't find the communication you are trying to edit")
+      throw new UpdateException('Can\'t find the communication you are trying to edit')
     }
     
     const query = {
-      "$push": {
+      '$push': {
         messages: {
           sender: this.authUser,
           content: addMessageCommunicationDto.message,
-          attachments: addMessageCommunicationDto.attachments
+          attachments: addMessageCommunicationDto.attachments,
+          type
         }
       }
     }
@@ -91,13 +94,33 @@ export class CommunicationsService {
       
       return acc
     }, [])
-    
+  
     if (filesToDelete.length > 0) {
       const deleteResult = await this.filesService.delete(filesToDelete)
     }
-    
+  
     await toRemove.delete()
-    
+  
     return
+  }
+  
+  /**
+   * Function that removes a single message.
+   * This should be used to remove automatic messages, because will not remove eventual attachments.
+   */
+  async removeMessage (id: string): Promise<void> {
+    const communication: CommunicationDocument = await this.communicationModel.findOne({ 'messages._id': castToObjectId(id) })
+    
+    if (!communication) {
+      throw new FindException()
+    }
+    
+    const query = {
+      '$pull': {
+        'messages': { '_id': castToObjectId(id) }
+      }
+    }
+    
+    await communication.update(query)
   }
 }
