ff10223ad6bdb12f625bd9b6869cc96a400233a7
--get <florian.leica@gmail.com>
2021-10-05 18:40:18 +0200

    Adds type prop to Messages schema

--- src/communications/schemas/messsage.schema.ts
+++ src/communications/schemas/messsage.schema.ts
@@ -2,6 +2,7 @@ import {Attachment, AttachmentSchema} from "../../_schemas/attachment.schema";
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
 import {UserBasic, UserBasicSchema} from "../../users/schemas/user-basic.schema";
 import {Document} from "mongoose";
+import {MessageTypeEnum} from "../enums/message.type.enum";
 
 @Schema({
   timestamps: true
@@ -15,6 +16,9 @@ export class Message extends Document {
   
   @Prop({required: true})
   content: string
+  
+  @Prop({default: MessageTypeEnum.MESSAGE, enum: MessageTypeEnum})
+  type: string
 }
 
 export const MessageSchema = SchemaFactory.createForClass(Message)
