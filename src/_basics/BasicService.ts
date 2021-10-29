f92d7cb73beb358be8532519bf79f839c25813ab
leicaflorian <florian.leica@gmail.com>
2021-10-29 18:32:10 +0200

    Adds pagination logic

--- src/_basics/BasicService.ts
+++ src/_basics/BasicService.ts
@@ -1,9 +1,35 @@
-import {FindException} from "../_exceptions/find.exception";
+import { FindException } from "../_exceptions/find.exception";
+import { Document, Model, Query, QueryOptions } from 'mongoose';
+
+export enum PaginationOrderEnum {
+  ASC = "ASC",
+  DESC = 'DESC'
+}
+
+export interface PaginationOptions {
+  page?: number;
+  perPage?: number;
+  sortBy?: string[];
+  order?: PaginationOrderEnum
+}
+
+export interface PaginatedResult<T = any> extends PaginationOptions {
+  data: T;
+  totalPages: number;
+  totalItems: number
+}
+
+const defaultPaginationOptions: PaginationOptions = {
+  page: 1,
+  perPage: 30,
+  sortBy: ["_id"],
+  order: PaginationOrderEnum.ASC
+}
 
 export abstract class BasicService {
-  abstract model: any;
+  abstract model: Model<any>;
   
-  protected async findOrFail<T>(id: string): Promise<T> {
+  protected async findOrFail<T> (id: string): Promise<T> {
     const item = await this.model.findById(id)
     
     if (!item) {
@@ -11,5 +37,45 @@ export abstract class BasicService {
     }
     
     return item
+  }
+  
+  /**
+   * Make a find query, paginating the result
+   *
+   * @protected
+   */
+  protected async findPaginated<T> (filter: any,
+    paginationOptions?: PaginationOptions,
+    projection?: any, options?: QueryOptions): Promise<PaginatedResult<T[]>> {
+    
+    const sortOptions: PaginationOptions = Object.assign({}, defaultPaginationOptions, paginationOptions);
+    
+    const opts: QueryOptions = {
+      skip: sortOptions.page <= 1 ? 0 : sortOptions.page * sortOptions.perPage,
+      limit: sortOptions.perPage,
+      sort: sortOptions.sortBy.reduce((acc, curr) => {
+        acc[curr] = sortOptions.order === "ASC" ? 1 : -1;
+        
+        return acc
+      }, {}),
+      
+    }
+    
+    // If there are other options, merge them
+    if (options) {
+      Object.assign(opts, options)
+    }
+    
+    console.log("paginating data", opts)
+    
+    const count: number = await this.model.find(filter, projection, options).count().exec()
+    const data: T[] = await this.model.find(filter, projection, opts).exec()
+    
+    return {
+      ...sortOptions,
+      totalItems: count,
+      totalPages: Math.floor(count / paginationOptions.perPage),
+      data
+    }
   }
 }
---
+++ src/_basics/pagination.dto.ts
@@ -0,0 +1,32 @@
+import { PaginatedResult, PaginationOrderEnum } from './BasicService';
+import { IsArray, IsEnum, IsNumber, IsOptional, IsString, Max, Min, } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class PaginatedFilterDto<T = any> implements Pick<PaginatedResult, "order" | "page" | "perPage"> {
+  @IsOptional()
+  @IsArray()
+  @Transform(({ value }) => (value instanceof Array) ? value : [value])
+  sortBy?: string[] = ["_id"];
+  
+  @IsOptional()
+  @IsString()
+  @IsEnum(PaginationOrderEnum)
+  order?: PaginationOrderEnum = PaginationOrderEnum.ASC;
+  
+  @IsOptional()
+  @Transform(({ value }) => +value)
+  @IsNumber()
+  @Min(1)
+  page?: number = 1;
+  
+  @IsOptional()
+  @Transform(({ value }) => +value)
+  @IsNumber()
+  @Max(100)
+  @Min(10)
+  perPage?: number = 30;
+  
+  /*  @IsOptional()
+    @IsArray()
+    filter?: string[]*/
+}
