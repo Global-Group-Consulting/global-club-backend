4f93cca0f76d68ba61016c387b74aa2e5518e528
leicaflorian <florian.leica@gmail.com>
2021-11-11 01:23:44 +0100

    Updates dashboard calls description

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthRequest } from '../_basics/AuthRequest';
 import { BasicController } from '../_basics/BasicController';
 
@@ -12,7 +12,12 @@ export class DashboardController extends BasicController {
     super()
   }
   
-  @Get()
+  @ApiOperation({
+    description: `A user portfolio is divided by semesters (January-June, July-December).<br>
+    Each semester has a "total" key that indicates how much can still be used for that semester. There are also the expiration date and usable from date.<br>
+    Last there is a "packs" object which will contain all details for the remaining amount, based on the clubPack associated `
+  })
+  @Get("statistics")
   async read (@Req() req: AuthRequest) {
     if (this.dashboardService.userIsAdmin) {
       return this.dashboardService.readAdminDashboard()
