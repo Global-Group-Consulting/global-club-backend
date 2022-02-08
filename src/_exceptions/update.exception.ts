c059f008c6d52d2eb4c33365e55856813dd37fb4
leicaflorian <florian.leica@gmail.com>
2022-02-08 22:36:48 +0100

    Adds code to UpdateException

--- src/_exceptions/update.exception.ts
+++ src/_exceptions/update.exception.ts
@@ -1,7 +1,7 @@
 import {HttpException, HttpStatus} from "@nestjs/common";
 
 export class UpdateException extends HttpException {
-  constructor(message?: string) {
-    super(message || "Can't update the requested element.", HttpStatus.BAD_REQUEST);
+  constructor(message?: string, code?: number) {
+    super(message || "Can't update the requested element.", code || HttpStatus.BAD_REQUEST);
   }
 }
