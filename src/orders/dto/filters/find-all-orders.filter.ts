ec3316c094f9de45c327d12b9b024ad7afd6ab70
leicaflorian <florian.leica@gmail.com>
2022-04-29 18:58:46 +0200

    Updates returnes orders data

--- src/orders/dto/filters/find-all-orders.filter.ts
+++ src/orders/dto/filters/find-all-orders.filter.ts
@@ -1,35 +1,35 @@
-import {ApiProperty} from '@nestjs/swagger';
-import {OrderStatusEnum} from '../../enums/order.status.enum';
-import {IsEnum, IsNotEmpty, IsOptional, ValidateNested} from 'class-validator';
-import {FilterMap} from '../../../_basics/FilterMap.dto';
-import {toString} from '../../../_basics/transformers/toString';
-import {toStringArray} from '../../../_basics/transformers/toStringArray';
-import {ToArray} from '../../../_basics/transformers/toArray';
+import { ApiProperty } from '@nestjs/swagger'
+import { OrderStatusEnum } from '../../enums/order.status.enum'
+import { IsEnum, IsNotEmpty, IsOptional, ValidateNested } from 'class-validator'
+import { FilterMap } from '../../../_basics/FilterMap.dto'
+import { toString } from '../../../_basics/transformers/toString'
+import { toStringArray } from '../../../_basics/transformers/toStringArray'
+import { ToArray } from '../../../_basics/transformers/toArray'
 import { castToObjectId } from '../../../utilities/Formatters'
 import { toBoolean } from '../../../_basics/transformers/toBoolean'
 
 export class FindAllOrdersFilter {
   @ApiProperty({
-    name: "filter[status]",
+    name: 'filter[status]',
     enum: OrderStatusEnum,
-    example: "filter[status]=pending&filter[status]=inProgress",
+    example: 'filter[status]=pending&filter[status]=inProgress',
     isArray: true,
     required: false
   })
   @IsOptional()
   @ToArray()
-  @IsEnum(OrderStatusEnum, {each: true})
+  @IsEnum(OrderStatusEnum, { each: true })
   status?: OrderStatusEnum[]
   
   @ApiProperty({
-    name: "filter[userId]",
+    name: 'filter[userId]',
     required: false
   })
   @IsOptional()
-  "userId"?: string
+  'userId'?: string
   
   @ApiProperty({
-    name: "filter[user]",
+    name: 'filter[user]',
     required: false
   })
   @IsOptional()
@@ -48,15 +48,22 @@ export class FindAllOrdersFilter {
   })
   @IsOptional()
   'packChangeOrder'?: string
+  
+  @ApiProperty({
+    name: 'filter[prodCategory]',
+    required: false
+  })
+  @IsOptional()
+  'prodCategory'?: string
 }
 
 export const FindAllOrdersFilterMap: FilterMap<FindAllOrdersFilter> = {
   status: {
-    castValue: toStringArray,
+    castValue: toStringArray
   },
-  "userId": {
+  'userId': {
     // Serve aggiungere una funzione che permetta di cambiare anche il nome della chiave in quanto deve essere "user.id"
-    keyFormat: () => "user.id"
+    keyFormat: () => 'user.id'
     // castValue: toObjectId,
     // query: value => ({ "id": value })
   },
@@ -70,5 +77,6 @@ export const FindAllOrdersFilterMap: FilterMap<FindAllOrdersFilter> = {
   },
   'packChangeOrder': {
     castValue: toBoolean
-  }
+  },
+  'prodCategory': {}
 }
