4efb22ecdcecf7606700d97ea7c92f88edf1722c
leicaflorian <florian.leica@gmail.com>
2022-05-09 21:26:33 +0200

    Adds originalValue to keyFormat function

--- src/_basics/FilterMap.dto.ts
+++ src/_basics/FilterMap.dto.ts
@@ -1,7 +1,7 @@
 export interface FilterOptions {
   query?: (value: any) => string | RegExp | number | boolean | object;
   castValue?: (value: any) => any;
-  keyFormat?: (value: string) => string;
+  keyFormat?: (value: string, elValue: any) => string;
 }
 
 export type FilterMap<T> = Record<keyof T, FilterOptions>
