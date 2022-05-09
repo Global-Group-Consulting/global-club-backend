b80c74ec1df8dfa922d028103a8a1d38200decf9
leicaflorian <florian.leica@gmail.com>
2022-05-09 21:28:46 +0200

    Adds parent and level to product-category.schema.ts

--- src/product-category/product-category.controller.ts
+++ src/product-category/product-category.controller.ts
@@ -1,53 +1,54 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, Query} from '@nestjs/common';
-import {ProductCategoryService} from './product-category.service';
-import {CreateProductCategoryDto} from './dto/create-product-category.dto';
-import {UpdateProductCategoryDto} from './dto/update-product-category.dto';
-import {ApiBasicAuth, ApiBearerAuth, ApiTags} from "@nestjs/swagger";
-import {ReadDto} from "../_basics/read.dto";
-import {ProductCategory} from './schemas/product-category.schema';
-import {PaginatedFindAllProductCategoriesDto} from "./dto/paginated-find-all-product-categories.dto";
-import {PaginatedResultProductCategoryDto} from "./dto/paginated-result-product-category.dto";
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common'
+import { ProductCategoryService } from './product-category.service'
+import { CreateProductCategoryDto } from './dto/create-product-category.dto'
+import { UpdateProductCategoryDto } from './dto/update-product-category.dto'
+import { ApiBasicAuth, ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { ReadDto } from '../_basics/read.dto'
+import { ProductCategory } from './schemas/product-category.schema'
+import { PaginatedFindAllProductCategoriesDto } from './dto/paginated-find-all-product-categories.dto'
+import { PaginatedResultProductCategoryDto } from './dto/paginated-result-product-category.dto'
+import { FindAllRawProductCategoriesDto } from './dto/find-all-raw-product-categories.dto'
 
 @ApiBearerAuth()
-@ApiBasicAuth("client-key")
-@ApiTags("ProductCategories")
+@ApiBasicAuth('client-key')
+@ApiTags('ProductCategories')
 @Controller('product-categories')
 export class ProductCategoryController {
-  constructor(private readonly productCategoryService: ProductCategoryService) {
+  constructor (private readonly productCategoryService: ProductCategoryService) {
   }
   
   @Post()
-  create(@Body() createProductCategoryDto: CreateProductCategoryDto) {
-    return this.productCategoryService.create(createProductCategoryDto);
+  create (@Body() createProductCategoryDto: CreateProductCategoryDto) {
+    return this.productCategoryService.create(createProductCategoryDto)
   }
   
   @Get()
-  findAll(@Query() paginationQuery: PaginatedFindAllProductCategoriesDto): Promise<PaginatedResultProductCategoryDto> {
-    return this.productCategoryService.findAll(paginationQuery);
+  findAll (@Query() paginationQuery: PaginatedFindAllProductCategoriesDto): Promise<PaginatedResultProductCategoryDto> {
+    return this.productCategoryService.findAll(paginationQuery)
   }
   
-  @Get("/raw")
-  findAllRaw(): Promise<ProductCategory[]> {
-    return this.productCategoryService.findAllRaw();
+  @Get('/raw')
+  findAllRaw (@Query() query: FindAllRawProductCategoriesDto): Promise<ProductCategory[]> {
+    return this.productCategoryService.findAllRaw(query)
   }
   
   @Get(':id')
-  findOne(@Param() params: ReadDto): Promise<ProductCategory> {
-    return this.productCategoryService.findOne(params.id);
+  findOne (@Param() params: ReadDto): Promise<ProductCategory> {
+    return this.productCategoryService.findOne(params.id)
   }
   
   @Patch(':id')
-  update(@Param() params: ReadDto, @Body() updateProductCategoryDto: UpdateProductCategoryDto): Promise<ProductCategory> {
-    return this.productCategoryService.update(params.id, updateProductCategoryDto);
+  update (@Param() params: ReadDto, @Body() updateProductCategoryDto: UpdateProductCategoryDto): Promise<ProductCategory> {
+    return this.productCategoryService.update(params.id, updateProductCategoryDto)
   }
   
   @Delete(':id')
-  remove(@Param() params: ReadDto) {
-    return this.productCategoryService.remove(params.id);
+  remove (@Param() params: ReadDto) {
+    return this.productCategoryService.remove(params.id)
   }
   
   @Delete(':id/thumbnail')
   removeThumbnail (@Param() params: ReadDto): Promise<ProductCategory> {
-    return this.productCategoryService.removeThumbnail(params.id);
+    return this.productCategoryService.removeThumbnail(params.id)
   }
 }
--- src/product-category/schemas/product-category.schema.ts
+++ src/product-category/schemas/product-category.schema.ts
@@ -1,28 +1,36 @@
-import {Document, Types} from "mongoose";
-import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
-import {BasicSchema} from "../../_schemas/BasicSchema";
-import {Attachment, AttachmentSchema} from "../../_schemas/attachment.schema";
+import { Document, Schema as MongoSchema, Types } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { BasicSchema } from '../../_schemas/BasicSchema'
+import { Attachment, AttachmentSchema } from '../../_schemas/attachment.schema'
 
 export type ProductCategoryDocument = ProductCategory & Document;
 
 @Schema({
   timestamps: true,
-  collection: "product_categories"
+  collection: 'product_categories'
 })
 export class ProductCategory extends BasicSchema {
   
-  @Prop({required: true})
-  title: string;
+  @Prop({ required: true })
+  title: string
   
-  @Prop({required: true})
-  description: string;
+  @Prop({ required: true })
+  description: string
   
-  @Prop({type: AttachmentSchema})
-  thumbnail: Attachment;
+  @Prop({ type: AttachmentSchema })
+  thumbnail: Attachment
   
-  _id: Types.ObjectId;
-  createdAt: Date;
-  updatedAt: Date;
+  @Prop({ type: MongoSchema.Types.ObjectId, ref: 'ProductCategory' })
+  parent: ProductCategory
+  
+  @Prop({ default: 0 })
+  level: number
+  
+  levels: number[]
+  
+  _id: Types.ObjectId
+  createdAt: Date
+  updatedAt: Date
 }
 
 export const ProductCategorySchema = SchemaFactory.createForClass(ProductCategory)
