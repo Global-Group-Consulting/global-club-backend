765b06648184e5398687ed3e0f0dc99e91814adc
= <=>
2022-07-09 11:35:55 +0200

    Updates Laravel Queue for using mysql pool

--- src/config/queue.config.ts
+++ src/config/queue.config.ts
@@ -1,20 +1,21 @@
-import {registerAs} from "@nestjs/config";
-import {LaravelQueueConfig} from "../queue/LaravelQueue";
-import * as fs from "fs";
+import { registerAs } from '@nestjs/config'
+import { LaravelQueueConfig } from '../queue/LaravelQueue'
+import * as fs from 'fs'
 
-function ssl(): any {
+function ssl (): any {
   if (process.env.QUEUE_SSL_CERT) {
     return {
       rejectUnauthorized: true,
-      ca: fs.readFileSync(__dirname + '/../../' + process.env.QUEUE_SSL_CERT),
+      ca: fs.readFileSync(__dirname + '/../../' + process.env.QUEUE_SSL_CERT)
     }
   }
   
   return null
 }
 
-export default registerAs("queue", () => ({
+export default registerAs('queue', () => ({
   db: {
+    connectionLimit: 10,
     host: process.env.QUEUE_HOST,
     port: +process.env.QUEUE_PORT ?? null,
     user: process.env.QUEUE_USER,
